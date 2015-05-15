07f9bea5e65fb06fa4b441b084970e9dfaade5c8
Miriam Nadler <kennedy.nadler@gmail.com>
2015-05-15 02:54:18 -0400

    adds modules: line, page-wrap, textbox, title

--- wp-content/themes/quiltbag/page-styleguide.php
+++ wp-content/themes/quiltbag/page-styleguide.php
@@ -6,12 +6,8 @@
 
 <?php get_header(); ?>
 
-<div class="apple">Queer and trans and engaged in tech.</div>
-<div class="textbox">
+<div class="m-textbox">
   <p>The quick brown fox jumps lazily over the red dog. Six hexing wizards win the untoward quiz show. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
 </div>
-
-<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-
 
 <?php get_footer(); ?>
