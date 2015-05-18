81e0e5fa107f5996623c9b4ea2d21aefa5adea14
Robyn <robynover@gmail.com>
2015-05-18 10:51:03 -0400

    Edited static text of secondary tagline

--- wp-content/themes/quiltbag/page-home.php
+++ wp-content/themes/quiltbag/page-home.php
@@ -74,7 +74,7 @@
            </section>
            <section class="l-rail">
              <div class="m-box" --xs>
-              <p>this group is based in <span class="emphasis">NYC</span></p>
+              <p><span class="emphasis">NYC Queer Tech</span></p>
              </div>
              <!--
             <div class="m-box m-box - - nav"> //fix dashes if you uncomment
